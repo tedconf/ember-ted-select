3f6d170f5cf9e68f4a1ec12549ac41ebef2cfe75
Brenna <info@brennaobrien.com>
2015-06-18 18:30:50 -0400

    move styles into /vendor/ to be bundled with addon

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-ted-select'
+  name: 'ember-ted-select',
+
+  included: function(app) {
+     app.import('vendor/ember-ted-select/searchable-select.css');
+  }
+
 };
